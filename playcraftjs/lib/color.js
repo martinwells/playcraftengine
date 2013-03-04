bfb42b4644dd0b42ce278fbc88f8190b68489f46
martinwells <martin@playcraftlabs.com>
2013-03-03 23:11:40 -0800

    defaults with no color specified (so you can just do pc.Rect.create({});

--- playcraftjs/lib/color.js
+++ playcraftjs/lib/color.js
@@ -22,188 +22,190 @@
  * </code></pre>
  */
 pc.Color = pc.Pooled.extend('pc.Color',
-    /** @lends pc.Color */
+  /** @lends pc.Color */
+  {
+    /**
+     * Constructs a color object using the passed in color
+     * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
+     * green, blue levels, i.e. full red is [255, 0, 0]
+     */
+    create: function (color)
     {
-        /**
-         * Constructs a color object using the passed in color
-         * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
-         * green, blue levels, i.e. full red is [255, 0, 0]
-         */
-        create:function (color)
-        {
-            var n = this._super();
-            n.config(color);
-            return n;
-        }
+      var n = this._super();
+      n.config(color);
+      return n;
+    }
+  },
+  /** @lends pc.Color.prototype */
+  {
+    /** Array of current colors */
+    rgb: [],
+    /** Representation of the current color as an rgb string. Automatically updates as you change color */
+    color: null,
+
+    /**
+     * Constructs a new color using the passed in color string. Used if you call new pc.Color, but typically
+     * you should be using pc.Color.create as this is a pooled class
+     * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
+     * green, blue levels, i.e. full red is [255, 0, 0]
+     */
+    init: function (color)
+    {
+      if (color != undefined)
+        this.config(color);
     },
-    /** @lends pc.Color.prototype */
+
+    /**
+     * Configure this color object with a given color
+     * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
+     * green, blue levels, i.e. full red is [255, 0, 0]
+     */
+    config: function (color)
     {
-        /** Array of current colors */
-        rgb:[],
-        /** Representation of the current color as an rgb string. Automatically updates as you change color */
-        color:null,
+      if (!color) return;
 
-        /**
-         * Constructs a new color using the passed in color string. Used if you call new pc.Color, but typically
-         * you should be using pc.Color.create as this is a pooled class
-         * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
-         * green, blue levels, i.e. full red is [255, 0, 0]
-         */
-        init:function (color)
+      if (Array.isArray(color))
+        this.rgb = color;
+      else
+      {
+        if (color.charAt(0) === '#')
         {
-            if (color != undefined)
-                this.config(color);
-        },
+          this.rgb[0] = parseInt(color.substring(1, 3), 16);
+          this.rgb[1] = parseInt(color.substring(3, 5), 16);
+          this.rgb[2] = parseInt(color.substring(5, 7), 16);
+        } else
+          throw "Invalid color: use either array [r,g,b] or '#RRGGBB'";
+      }
+      this._updateColorCache();
+    },
 
-        /**
-         * Configure this color object with a given color
-         * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
-         * green, blue levels, i.e. full red is [255, 0, 0]
-         */
-        config:function (color)
-        {
-            if (Array.isArray(color))
-                this.rgb = color;
-            else
-            {
-                if (color.charAt(0) === '#')
-                {
-                    this.rgb[0] = parseInt(color.substring(1, 3), 16);
-                    this.rgb[1] = parseInt(color.substring(3, 5), 16);
-                    this.rgb[2] = parseInt(color.substring(5, 7), 16);
-                } else
-                    throw "Invalid color: use either array [r,g,b] or '#RRGGBB'";
-            }
-            this._updateColorCache();
-        },
+    /**
+     * Sets this color object to a given color (synonym for config(color)
+     * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
+     * green, blue levels, i.e. full red is [255, 0, 0]
+     */
+    set: function (color)
+    {
+      this.config(color);
+    },
 
-        /**
-         * Sets this color object to a given color (synonym for config(color)
-         * @param {Array|String} color Can either be a string in the form of #RRGGBB or an array of 3 numbers representing red,
-         * green, blue levels, i.e. full red is [255, 0, 0]
-         */
-        set:function (color)
-        {
-            this.config(color);
-        },
+    /**
+     * Sets the red component of the color
+     * @param {Number} r Red component of the color to set
+     */
+    setRed: function (r)
+    {
+      this.rgb[0] = pc.Math.limit(r, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Sets the red component of the color
-         * @param {Number} r Red component of the color to set
-         */
-        setRed:function (r)
-        {
-            this.rgb[0] = pc.Math.limit(r, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Adds to the red component of the color
+     * @param {Number} r Red component
+     */
+    addRed: function (r)
+    {
+      this.rgb[0] = pc.Math.limitAdd(this.rgb[0], r, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Adds to the red component of the color
-         * @param {Number} r Red component
-         */
-        addRed:function (r)
-        {
-            this.rgb[0] = pc.Math.limitAdd(this.rgb[0], r, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Subtracts from the red component of the color
+     * @param {Number} r Red component
+     */
+    subRed: function (r)
+    {
+      this.rgb[0] = pc.Math.limitAdd(this.rgb[0], -r, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Subtracts from the red component of the color
-         * @param {Number} r Red component
-         */
-        subRed:function (r)
-        {
-            this.rgb[0] = pc.Math.limitAdd(this.rgb[0], -r, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Sets the green component of the color
+     * @param {Number} g Green component of the color to set
+     */
+    setGreen: function (g)
+    {
+      this.rgb[1] = pc.Math.limit(g, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Sets the green component of the color
-         * @param {Number} g Green component of the color to set
-         */
-        setGreen:function (g)
-        {
-            this.rgb[1] = pc.Math.limit(g, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Adds to the green component of the color
+     * @param {Number} g Green component
+     */
+    addGreen: function (g)
+    {
+      this.rgb[1] = pc.Math.limitAdd(this.rgb[1], g, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Adds to the green component of the color
-         * @param {Number} g Green component
-         */
-        addGreen:function (g)
-        {
-            this.rgb[1] = pc.Math.limitAdd(this.rgb[1], g, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Subtracts from the green component of the color
+     * @param {Number} g Green component
+     */
+    subGreen: function (g)
+    {
+      this.rgb[1] = pc.Math.limitAdd(this.rgb[1], -g, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Subtracts from the green component of the color
-         * @param {Number} g Green component
-         */
-        subGreen:function (g)
-        {
-            this.rgb[1] = pc.Math.limitAdd(this.rgb[1], -g, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Sets the blue component of the color
+     * @param {Number} b Blue component of the color to set
+     */
+    setBlue: function (b)
+    {
+      this.rgb[2] = pc.Math.limit(b, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Sets the blue component of the color
-         * @param {Number} b Blue component of the color to set
-         */
-        setBlue:function (b)
-        {
-            this.rgb[2] = pc.Math.limit(b, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Adds to the blue component of the color
+     * @param {Number} b Blue component of the color to set
+     */
+    addBlue: function (b)
+    {
+      this.rgb[2] = pc.Math.limitAdd(this.rgb[2], b, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Adds to the blue component of the color
-         * @param {Number} b Blue component of the color to set
-         */
-        addBlue:function (b)
-        {
-            this.rgb[2] = pc.Math.limitAdd(this.rgb[2], b, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Subtracts from the blue component of the color
+     * @param {Number} b Blue component
+     */
+    subBlue: function (b)
+    {
+      this.rgb[2] = pc.Math.limitAdd(this.rgb[2], -b, 0, 255);
+      this._updateColorCache();
+    },
 
-        /**
-         * Subtracts from the blue component of the color
-         * @param {Number} b Blue component
-         */
-        subBlue:function (b)
-        {
-            this.rgb[2] = pc.Math.limitAdd(this.rgb[2], -b, 0, 255);
-            this._updateColorCache();
-        },
+    /**
+     * Darkens the color by the given value (1..255)
+     * @param {Number} amt Amount to darken the color by
+     */
+    darken: function (amt)
+    {
+      this.subRed(amt);
+      this.subGreen(amt);
+      this.subBlue(amt);
+    },
 
-        /**
-         * Darkens the color by the given value (1..255)
-         * @param {Number} amt Amount to darken the color by
-         */
-        darken:function (amt)
-        {
-            this.subRed(amt);
-            this.subGreen(amt);
-            this.subBlue(amt);
-        },
+    /**
+     * Lightens the color by the given amount (1..255)
+     * @param {Number} amt Amount to lighten the color by
+     */
+    lighten: function (amt)
+    {
+      this.addRed(amt);
+      this.addGreen(amt);
+      this.addBlue(amt);
+    },
 
-        /**
-         * Lightens the color by the given amount (1..255)
-         * @param {Number} amt Amount to lighten the color by
-         */
-        lighten:function (amt)
-        {
-            this.addRed(amt);
-            this.addGreen(amt);
-            this.addBlue(amt);
-        },
-
-        _updateColorCache:function ()
-        {
-            // todo: this is constructing a string on every adjustment: can we save on that?
-            this.color = 'rgb(' + this.rgb[0] + ',' + this.rgb[1] + ',' + this.rgb[2] + ')';
-        }
+    _updateColorCache: function ()
+    {
+      // todo: this is constructing a string on every adjustment: can we save on that?
+      this.color = 'rgb(' + this.rgb[0] + ',' + this.rgb[1] + ',' + this.rgb[2] + ')';
+    }
 
 
-    });
+  });
--- playcraftjs/lib/components/rect.js
+++ playcraftjs/lib/components/rect.js
@@ -18,66 +18,70 @@
  * </code></pre>
  */
 pc.components.Rect = pc.components.Component.extend('pc.components.Rect',
-    /** @lends pc.components.Rect */
+  /** @lends pc.components.Rect */
+  {
+    /**
+     * Constructs (or acquires from the pool) a rectangle component.
+     * @param {String} options.color Fill color in the form of #RRGGBB.
+     * @param {String} options.lineColor Line color in the form of #RRGGBB
+     * @param {Number} options.lineWidth Stroke width
+     * @param {Number} options.cornerRadius Radius of the corners (defaults to 0)
+     * @return {pc.components.Rect} A rectangle component
+     */
+    create: function (options)
     {
-        /**
-         * Constructs (or acquires from the pool) a rectangle component.
-         * @param {String} options.color Fill color in the form of #RRGGBB.
-         * @param {String} options.lineColor Line color in the form of #RRGGBB
-         * @param {Number} options.lineWidth Stroke width
-         * @param {Number} options.cornerRadius Radius of the corners (defaults to 0)
-         * @return {pc.components.Rect} A rectangle component
-         */
-        create:function (options)
-        {
-            var n = this._super();
-            n.config(options);
-            return n;
-        }
+      var n = this._super();
+      n.config(options);
+      return n;
+    }
+  },
+  /** @lends pc.components.Rect.prototype */
+  {
+    /** pc.Color representing fill color */
+    color: null,
+    /** pc.Color representing stroke color */
+    lineColor: null,
+    /** Stroke width */
+    lineWidth: 0,
+    /** radius of the corners (0=straight edges) */
+    cornerRadius: 0,
+
+    /**
+     * Constructs a new component. See create method for options
+     * @param {Object} options Options
+     */
+    init: function (options)
+    {
+      this._super('rect');
+      this.color = null;
+      this.lineColor = null;
+      if (pc.valid(options))
+        this.config(options);
     },
-    /** @lends pc.components.Rect.prototype */
+
+    /**
+     * Configures the component. See create method for options
+     * @param {Object} options Options
+     */
+    config: function (options)
     {
-        /** pc.Color representing fill color */
-        color:null,
-        /** pc.Color representing stroke color */
-        lineColor:null,
-        /** Stroke width */
-        lineWidth:0,
-        /** radius of the corners (0=straight edges) */
-        cornerRadius:0,
+      if (options.color)
+        this.color.set(options.color); // can be null
+      else
+        this.color = null;
 
-        /**
-         * Constructs a new component. See create method for options
-         * @param {Object} options Options
-         */
-        init:function (options)
-        {
-            this._super('rect');
-            this.color = pc.Color.create('#ffffff');
-            this.lineColor = pc.Color.create('#888888');
-            if (pc.valid(options))
-                this.config(options);
-        },
+      if (options.lineColor)
+      {
+        if (!this.lineColor)
+          this.lineColor = pc.Color.create();
 
-        /**
-         * Configures the component. See create method for options
-         * @param {Object} options Options
-         */
-        config:function (options)
-        {
-            if (!options.color)
-                this.color.set('#ffffff');
-            else
-                this.color.set(pc.checked(options.color, '#ffffff'));
+        this.lineColor.set(options.lineColor);
+      } else
+        this.lineColor = null;
 
-            if (!options.lineColor)
-                this.lineColor.set('#ffffff');
-            else
-                this.lineColor.set(pc.checked(options.lineColor, '#888888'));
+      this.lineWidth = pc.checked(options.lineWidth, 1);
+      this.cornerRadius = pc.checked(options.cornerRadius, 0);
+    }
 
-            this.lineWidth = pc.checked(options.lineWidth, 0);
-            this.cornerRadius = pc.checked(options.cornerRadius, 0);
-        }
-
-    });
+  });
 
