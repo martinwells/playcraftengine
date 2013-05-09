302b253405b3c0fa26cad20de3cc6966e5dee8f7
martinwells <martin@playcraftlabs.com>
2013-05-09 15:22:17 -0700

    some reformatting

--- playcraftjs/lib/components/alpha.js
+++ playcraftjs/lib/components/alpha.js
@@ -12,65 +12,77 @@
  * Changes the alpha drawing of an associated drawable object (sprite, shape, text etc).
  */
 pc.components.Alpha = pc.components.Component.extend('pc.components.Alpha',
-    /** @lends pc.components.Alpha */
+  /** @lends pc.components.Alpha */
+  {
+    /**
+     * Constructs (or acquires) an alpha component.
+     * @param {Number} options.level Amount of initial alpha to set
+     * @return {pc.components.Alpha} The new alpha object
+     */
+    create: function (options)
     {
-        /**
-         * Constructs (or acquires) an alpha component.
-         * @param {Number} options.level Amount of initial alpha to set
-         * @return {pc.components.Alpha} The new alpha object
-         */
-        create:function (options)
-        {
-            var n = this._super();
-            n.config(options);
-            return n;
-        }
+      var n = this._super();
+      n.config(options);
+      return n;
+    }
+  },
+  /** @lends pc.components.Alpha.prototype */
+  {
+    /** Current alpha level 0=fully transparent */
+    level: 1,
+
+    /**
+     * Constructs a new component. See create method for options
+     */
+    init: function ()
+    {
+      this._super('alpha');
     },
-    /** @lends pc.components.Alpha.prototype */
+
+    /**
+     * Configures the component. See create method for options
+     * @param {Object} options Options
+     */
+    config: function (options)
     {
-        /** Current alpha level 0=fully transparent */
-        level:1,
+      this.level = pc.checked(options.level, 1);
+    },
 
-        /**
-         * Constructs a new component. See create method for options
-         */
-        init:function()
-        {
-            this._super('alpha');
-        },
+    /**
+     * Set the alpha level
+     * @param {Number} a Level to set alpha to
+     */
+    setAlpha: function (a)
+    {
+      this.level = a;
+      this._fix(this.level);
+    },
 
-        /**
-         * Configures the component. See create method for options
-         * @param {Object} options Options
-         */
-        config:function (options)
-        {
-            this.level = pc.checked(options.level, 1);
-        },
+    /**
+     * Add to the alpha level
+     * @param {Number} a Amount to increase alpha by
+     */
+    addAlpha: function (a)
+    {
+      this.level += a;
+      this._fix(this.level);
+    },
 
-        /**
-         * Set the alpha level
-         * @param {Number} a Level to set alpha to
-         */
-        setAlpha: function(a)   { this.level = a;  this._fix(this.level); },
+    /**
+     * Subtract from the alpha level
+     * @param {Number} a Amount o subtract
+     */
+    subAlpha: function (a)
+    {
+      this.level -= a;
+      this._fix(this.level);
+    },
 
-        /**
-         * Add to the alpha level
-         * @param {Number} a Amount to increase alpha by
-         */
-        addAlpha: function(a)   { this.level += a; this._fix(this.level); },
+    _fix: function (c)
+    {
+      if (c > 1) return;
+      if (c < 0) return;
+      this.level = c;
+    }
 
-        /**
-         * Subtract from the alpha level
-         * @param {Number} a Amount o subtract
-         */
-        subAlpha: function(a)   { this.level -= a; this._fix(this.level); },
-
-        _fix: function(c)
-        {
-            if (c > 1) return;
-            if (c < 0) return;
-            this.level = c;
-        }
-
-    });
+  });
