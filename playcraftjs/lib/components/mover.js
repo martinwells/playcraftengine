8ce748eb1ce0f843a33755d90824f17eed66a436
martinwells <martin@playcraftlabs.com>
2013-05-10 20:59:27 -0700

    adds mover component (early version)

---
+++ playcraftjs/lib/components/mover.js
@@ -0,0 +1,38 @@
+
+
+// Moves an entity to a target location over time
+Mover = pc.components.Component.extend('Mover',
+  {
+    create: function (options)
+    {
+      var n = this._super();
+      n.config(options);
+      return n;
+    }
+  },
+  {
+    targetPos:0, // where am I headed
+    time:0, // time frame to move over (how fast we move)
+    onComplete: null, // optional callback when completed movement
+
+    _timeLeft: 0,
+
+    init:function (options)
+    {
+      this._super('mover');
+      if (pc.valid(options))
+        this.config(options);
+    },
+
+    config:function (options)
+    {
+      this.targetPos = options.targetPos;
+      this.time = pc.checked(options.time, 500);
+      this.onComplete = options.onComplete;
+      this._timeLeft = this.time;
+    }
+  });
+
+
+
+
---
+++ playcraftjs/lib/systems/mover.js
@@ -0,0 +1,46 @@
+pc.systems.Mover = pc.systems.EntitySystem.extend('MoverSystem',
+  {},
+  {
+    numMoving: 0,
+
+    init:function ()
+    {
+      this._super(['mover']);
+      this.numMoving = 0;
+    },
+
+    processAll: function()
+    {
+      // reset the number of moving entities
+      this.numMoving = 0;
+      // this will call process for every entity
+      this._super();
+    },
+
+    process:function (entity)
+    {
+      var mover = entity.getComponent('mover');
+      var spatial = entity.getComponent('spatial');
+
+      // how far left to move
+      var distancePerMS = spatial.pos.distance(mover.targetPos) / mover.time;
+
+      var dir = spatial.pos.dirTo(mover.targetPos);
+      spatial.pos.moveInDir(dir, distancePerMS * pc.device.elapsed);
+      mover._timeLeft -= pc.device.elapsed;
+
+      if (mover._timeLeft <= 0)
+      {
+        mover.active = false;
+        if (mover.onComplete)
+          mover.onComplete(entity);
+        entity.removeComponent(mover);
+      }
+      else
+      // this is reset every cycle by processAll
+        this.numMoving++;
+    }
+
+  });
+
+
